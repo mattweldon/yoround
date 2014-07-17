dc0b48199e45c5b68e7e14f5e9d0c94899de6720
Matt Weldon <matt.weldon@gmail.com>
2014-07-17 14:36:05 +0100

    working version

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 $: << File.expand_path(File.dirname(__FILE__))
 
 require './tea'
-run Sinatra::Application
+run Tea.new
--- tea.rb
+++ tea.rb
@@ -1,38 +1,50 @@
 require 'sinatra'
 require 'faraday'
 
-configure do
-  @round = []
-end 
+class Tea < Sinatra::Application
+  configure do
+    set :round, []
+    set :victim, 'no-one'
+  end 
 
-get '/' do
-  "The Strawerry Web team Tea app, yo!"
-end
-
-get '/thirsty' do
-
-  @round << params[:username]
-  
-  puts "members of the round: " + round_members.join(", ")
-
-  connection_yo.post('yo', { 
-    :api_token => 'c12adfbd-6778-bada-5f73-43ac72c95007', 
-    :username => round_members.sample # 
-  })
-  @round = []
-
-end
-
-private
-
-  def round_members
-    @round.uniq{ |x| x }
+  get '/' do
+    "The Strawerry Web team Tea app, yo!<br/><br/>The round consists of #{round_members.join(", ")}<br/><br/>It's #{settings.victim}'s turn to brew up."
   end
 
-  def connection_yo
-    Faraday.new(:url => 'http://api.justyo.co/') do |builder|
-      builder.request  :url_encoded
-      builder.response :logger
-      builder.adapter  Faraday.default_adapter
+  get '/thirsty' do
+
+    settings.round << params[:username]
+
+    puts "== MEMBERS OF THE ROUND: " + round_members.join(", ")
+
+    if round_members.count >= 4
+
+      victim = round_members.sample
+
+        puts "== 4 OR MORE MEMBERS IN THE ROUND, TIME FOR #{victim} TO MAKE TEA"
+
+        response = connection_yo.post('yo/', { 
+          'api_token' => 'c12adfbd-6778-bada-5f73-43ac72c95007', 
+          'username' =>  victim# 
+        }) 
+
+        puts "== YO RESPONSE: #{response.body}"
+
+        settings.round = []
     end
   end
+
+  private
+
+    def round_members
+      settings.round.uniq{ |x| x }
+    end
+
+    def connection_yo
+      Faraday.new(:url => 'http://api.justyo.co/') do |builder|
+        builder.request  :url_encoded
+        builder.response :logger
+        builder.adapter  Faraday.default_adapter
+      end
+    end
+end
